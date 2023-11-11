98d63029fcb506b7c6213b08848d6a41afaa4859
sim <sim30217@gmail.com>
2023-11-11 21:06:25 +0900

    greeting

--- src/main/java/com/pluralsight/conference/controller/GreetingController.java
+++ src/main/java/com/pluralsight/conference/controller/GreetingController.java
@@ -2,7 +2,7 @@ package com.pluralsight.conference.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import java.util.Map;
 @Controller
 
 public class GreetingController {
---
+++ src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Greeting
+        </title>
+    </head>
+    <body>
+        <h1>
+            ${message}
+        </h1>
+</html>
